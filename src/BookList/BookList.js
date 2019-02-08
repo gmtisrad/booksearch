67cb4fcbb0924a59198d7b874c1174dcd6824eb1
gmtisrad <gabe.m.timm@gmail.com>
2019-02-07 19:11:39 -0800

    Fixed price bug

--- src/BookList/BookList.js
+++ src/BookList/BookList.js
@@ -7,8 +7,8 @@ class BookList extends Component {
         let books = this.props.books.map(book => {
             console.log(book);
             return book ? <Book 
-                    key={book.id}
-                    price={book.saleInfo.amount ? book.saleInfo.listPrice.amount : 'Price Unavailable'}
+                    key={book.etag}
+                    price={book.saleInfo.listPrice ? book.saleInfo.listPrice.amount : 'Price Unavailable'}
                     title={book.volumeInfo.title} 
                     author={book.volumeInfo.authors ? book.volumeInfo.authors[0] : 'No Authors Listed'} 
                     src={book.volumeInfo.imageLinks ? book.volumeInfo.imageLinks.thumbnail : 'https://images.pexels.com/photos/45201/kitty-cat-kitten-pet-45201.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500'} 
